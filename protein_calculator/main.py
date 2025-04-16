d30a7962f52a5a78c5d424200d7f1b45431dcd7b
KBalcaen <kevin.balcaen22@gmail.com>
2025-04-16 14:52:42 +0200

    Flask exchanged for Argparse

--- protein_calculator/main.py
+++ protein_calculator/main.py
@@ -4,57 +4,36 @@
 # The tool is updated to usegalaxy.be ##
 ################################################################################################
 
-from json import dumps
-
+import argparse
 import pandas as pd
-from flask import Flask, render_template, request, session, redirect, url_for
+from json import dumps
 from plotly import utils, express as px
-
 from Calculate_protein_properties import (get_isoelectric_point, calculate_dn_dc, calculate_total_masses,
                                           calculate_extinction_coefficient, get_net_charge)
 from Sequence_functions import normalize_sequence, check_protein_sequence, format_sequence, letter_count
-from references import pK_values, amino_acid_data
+from references import amino_acid_data
 
-# Flask-application initialisation
-app = Flask(__name__)
+def main():
+    parser = argparse.ArgumentParser(description='Process some sequences.')
+    parser.add_argument('--name', type=str, required=True, help='Name of the protein')
+    parser.add_argument('--sequence', type=str, required=True, help='Sequence to be processed')
 
+    args = parser.parse_args()
 
-# Route for homepage with form
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        sequence = request.form['sequence'].upper()
+    name = args.name
+    sequence = args.sequence.upper()
 
-        # Normalise sequence to remove spaces and enters and check validity of sequence
-        sequence = normalize_sequence(sequence)
-
-        # Store necessary information in session
-        session['name'] = name
-        session['sequence'] = sequence
-
-        # Redirect to the results page
-        return redirect(url_for('results'))
-
-    return render_template('index.html')
-
-
-# Results route
-@app.route('/results', methods=["GET"])
-def results():
-    name = session.get('name')
-    sequence = session.get('sequence')
-
+    # Normaliseer de sequentie om spaties en enters te verwijderen en controleer de geldigheid van de sequentie
+    sequence = normalize_sequence(sequence)
     error_message = check_protein_sequence(sequence)
 
-    # Error Checking
     if error_message:
-        session["error_message"] = error_message
-        return redirect(url_for('calculator_error'))
+        print(f"Error: {error_message}")
+        return
 
     sequence_formatted = format_sequence(sequence, show_residue_number=True, line_length=55)
 
-    # Perform calculations
+    # Voer berekeningen uit
     frequency = letter_count(sequence)
     amino_acid_composition = []
     total_monoisotopic_mass = 18.0152
@@ -81,9 +60,7 @@ def results():
             'percentage': f"{percentage:.2f}%"
         })
 
-    number_info = {
-        'number': total_count
-    }
+    number_info = {'number': total_count}
 
     # Clean up of total_monoisotopic_mass and total_average_mass
     if total_monoisotopic_mass > 9999:
@@ -106,22 +83,18 @@ def results():
     molar_absorbance_info = {
         'extinction_coefficient_cystines': total_extinction_coefficient_cystines,
         'extinction_coefficient_reduced': total_extinction_coefficient_reduced,
-        'absorbance_mono_cystines': round(total_extinction_coefficient_cystines / total_monoisotopic_mass,
-                                          4) if total_monoisotopic_mass != 0 else 0,
-        'absorbance_avg_cystines': round(total_extinction_coefficient_cystines / total_average_mass,
-                                         4) if total_average_mass != 0 else 0,
-        'absorbance_mono_reduced': round(total_extinction_coefficient_reduced / total_monoisotopic_mass,
-                                         4) if total_monoisotopic_mass != 0 else 0,
-        'absorbance_avg_reduced': round(total_extinction_coefficient_reduced / total_average_mass,
-                                        4) if total_average_mass != 0 else 0
+        'absorbance_mono_cystines': round(total_extinction_coefficient_cystines / total_monoisotopic_mass, 4) if total_monoisotopic_mass != 0 else 0,
+        'absorbance_avg_cystines': round(total_extinction_coefficient_cystines / total_average_mass, 4) if total_average_mass != 0 else 0,
+        'absorbance_mono_reduced': round(total_extinction_coefficient_reduced / total_monoisotopic_mass, 4) if total_monoisotopic_mass != 0 else 0,
+        'absorbance_avg_reduced': round(total_extinction_coefficient_reduced / total_average_mass, 4) if total_average_mass != 0 else 0
     }
 
     pI = round(get_isoelectric_point(sequence), 2)
-
     net_charge_at_different_pH = []
     pH_values = []
     net_charges = []
     charges = get_net_charge(sequence)
+
     for pH, net_charge in charges.items():
         net_charge_at_different_pH.append({
             'pH': pH,
@@ -135,65 +108,23 @@ def results():
     colors = ['#3ABBBA', '#5A2A82', '#FF681E']
     titration_curve = px.line(df, x="pH", y="Net Charges", color_discrete_sequence=colors)
     titration_curve.update_layout(title_text="Titration Curve", title_x=0.5)
-
     titration_json = dumps(titration_curve, cls=utils.PlotlyJSONEncoder)
 
     dn_dc_value = round(calculate_dn_dc(sequence, amino_acid_data), 6)
 
-    # Put right variables in session
-    session["name"] = name
-    session['sequence'] = sequence
-    session['molecular_weight_info'] = molecular_weight_info
-    session['molar_absorbance_info'] = molar_absorbance_info
-    session['dn_dc_value'] = dn_dc_value
-    session['pI'] = pI
-    session['net_charge_at_different_pH'] = net_charge_at_different_pH
+    # Print Results
+    print(f'Name: {name}')
+    print(f'Sequence: {sequence_formatted}')
+    print(f'Amino Acid Composition:')
+    print(f'{"Amino Acid (Short)":<20} {"Amino Acid (Long)":<20} {"Monoisotopic Weight (Da)":<25} {"Average Weight (Da)":<25} {"# Counts":<10} {"% of Total":<10}')
+    for aa in amino_acid_composition:
+        print(f'{aa["amino_acid"]:<20} {aa["long_name"]:<20} {aa["mono_weight"]:<25} {aa["avg_weight"]:<25} {aa["count"]:<10} {aa["percentage"]:<10}')
+    print(f'Molecular Weight Info: {molecular_weight_info}')
+    print(f'Molar Absorbance Info: {molar_absorbance_info}')
+    print(f'pI: {pI}')
+    print(f'Net Charge at Different pH: {net_charge_at_different_pH}')
+    print(f'Titration Curve JSON: {titration_json}')
+    print(f'dn/dc Value: {dn_dc_value}')
 
-    return render_template('results.html',
-                           name=name,
-                           sequence=sequence_formatted,
-                           sequence2=sequence,
-                           amino_acid_composition=amino_acid_composition,
-                           number_info=number_info,
-                           molecular_weight_info=molecular_weight_info,
-                           molar_absorbance_info=molar_absorbance_info,
-                           pI=pI,
-                           net_charge_at_different_pH=net_charge_at_different_pH,
-                           titration_json=titration_json,
-                           dn_dc_value=dn_dc_value)
-
-
-# Export view page
-@app.route('/export_view', methods=["GET"])
-def export_data():
-    # Get session variables
-    name = session.get('name')
-    sequence = session.get('sequence')
-    molecular_weight_info = session.get('molecular_weight_info')
-    molar_absorbance_info = session.get('molar_absorbance_info')
-    dn_dc_value = session.get('dn_dc_value')
-    pI = session.get('pI')
-    net_charge_at_different_pH = session.get('net_charge_at_different_pH')
-
-    return render_template("export_view.html",
-                           name=name,
-                           sequence=sequence,
-                           molecular_weight_info=molecular_weight_info,
-                           molar_absorbance_info=molar_absorbance_info,
-                           dn_dc_value=dn_dc_value,
-                           pI=pI,
-                           net_charge_at_different_pH=net_charge_at_different_pH)
-
-
-@app.route('/calculator_error', methods=["GET"])
-def calculator_error():
-    error_message = session.get("error_message")
-    return render_template('protein_calc_error.html', error_message=error_message)
-
-
-# Start the Flask-application
 if __name__ == '__main__':
-    app.secret_key = "protein core"
-    app.config["SESSION_TYPE"] = "filesystem"
-
-    app.run(debug=False, port=8000, host="localhost")
+    main()
